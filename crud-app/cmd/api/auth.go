eee5f95765196c313d760db842fc917c4e0da387
La Min Ko Ko <rogermbl138998@gmail.com>
2025-04-22 13:45:21 +0700

    add jwt authorzation

--- crud-app/cmd/api/auth.go
+++ crud-app/cmd/api/auth.go
@@ -2,9 +2,11 @@ package main
 
 import (
 	"net/http"
+	"time"
 
 	"github.com/SIRIUS-webkit/crud-app/internal/database"
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt"
 	"golang.org/x/crypto/bcrypt"
 )	
 
@@ -46,4 +48,52 @@ func (app *application) registerUser(c *gin.Context){
 	
 }
 
+
+type LoginUserRequest struct {
+	Email string `json:"email" binding:"required,email"`
+	Password string `json:"password" binding:"required"`
+}
+
+type LoginUserResponse struct {
+	Token string `json:"token"`
+}
+
+func (app *application) loginUser(c *gin.Context){
+	var request LoginUserRequest
+
+	if err := c.ShouldBindJSON(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
+	}
+
+	user, err := app.models.Users.GetByEmail(request.Email)
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	err = bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(request.Password))
+	if err != nil {
+		c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid credentials"})
+		return
+	}
+
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+        "userId": user.Id,
+        "exp":    time.Now().Add(time.Hour * 72).Unix(), // Token expires in 72 hours
+    })
+
+    tokenString, err := token.SignedString([]byte(app.jwtSecret))
+    if err != nil {
+        c.JSON(http.StatusInternalServerError, gin.H{"error": "Error generating token"})
+        return
+    }
+
+    c.JSON(http.StatusOK, LoginUserResponse{Token: tokenString})
+
+}
+	
+	
+	
+	
 
